82aaeb2cf7308cde3ac0a60563e2e1f8a0d71177
Erich Smith <heartquest@gmail.com>
2015-01-09 13:59:53 -0500

    Refactor attempt
    @DavidIvy003: feel free to revert this commit if you hate it

--- roman-numerals.rb
+++ roman-numerals.rb
@@ -5,43 +5,67 @@ require 'colorize'
 class RomanNumerals
   def initialize(numeral)
     @input = Integer(numeral)
+    @value = ""
   end
 
   def to_s
-    value = ""
     while @input > 0
-      if @input / 90 == 1
-        value += "XC"
-        @input -= 90
+      append_value_XC
+      append_value_L
+      append_value_X
+      append_value_IX
+      append_value_V
+      append_value_IV_and_I
+    end
+    @value
+  end
+
+  def append_value_XC
+    if @input / 90 == 1
+     @value += "XC"
+     @input -= 90
+    end
+  end
+
+  def append_value_L
+    if @input / 50 == 1
+      @value += "L"
+      @input -= 50
+    end
+  end
+
+  def append_value_X
+    if @input >= 10
+      @value += "X"
+      @input -= 10
+    end
+  end
+
+  def append_value_IX
+    if @input == 9
+      @value += "IX"
+      @input -= 9
+    end
+  end
+
+  def append_value_V
+    if @input / 5 == 1
+      @value += "V"
+      @input -= 5
+    end
+  end
+
+  def append_value_IV_and_I
+    if @input < 5
+      if @input == 4
+        @value += "IV"
+        @input -= 4
       end
-      if @input / 50 == 1
-        value += "L"
-        @input -= 50
-      end
-      if @input >= 10
-        value += "X"
-        @input -= 10
-      end
-      if @input == 9
-        value += "IX"
-        @input -= 9
-      end
-      if @input / 5 == 1
-        value += "V"
-        @input -= 5
-      end
-      if @input < 5
-        if @input == 4
-          value += "IV"
-          @input -= 4
-        end
-        @input.times do
-          value += "I"
-          @input -= 1
-        end
+      @input.times do
+        @value += "I"
+        @input -= 1
       end
     end
-    value
   end
 end
 
